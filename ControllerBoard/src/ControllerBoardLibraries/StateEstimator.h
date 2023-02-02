4e362a35975a0b709989c7bb5785fe8d9ba870e7
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2023-02-01 21:59:33 -0400

    Getting things started

--- ControllerBoard/src/ControllerBoardLibraries/StateEstimator.h
+++ ControllerBoard/src/ControllerBoardLibraries/StateEstimator.h
@@ -25,20 +25,18 @@ class StateEstimator{
             4: gyro bias x
             5: gyro bias y
             6: gyro bias z
-            7: x position
-            8: y position
-            9: z position
-            10: x velocity
-            11: y velocity
-            12: z velocity
+            7: Altitude
+            8: Velocity North
+            9: Velocity East
+            10: Velocity Down
         */
-       Eigen::MatrixXf state = MatrixXf::Zero(13,1);
+       Eigen::MatrixXf state = MatrixXf::Zero(11,1);
        
        /*
             @brief Current state of the estimator
             @details The state of the estimator is used to determine what set of sensors/equations to use
             During PRELAUNCH, the IMU+Magnetometer+Barometer are used to estimate the full state using the classic EKF
-            During POSTLAUCH, the IMU+Magnetometer+Barometer are used to estimate the position, velocity, and orientation but the gyro bias is assumed to be constant throughout the flight
+            During POSTLAUCH, the IMU+Magnetometer+Barometer are used to estimate the velocity, and orientation but the gyro bias is assumed to be constant throughout the flight
        */
        EstimatorState E_state = PRELAUNCH;
 
@@ -49,9 +47,8 @@ class StateEstimator{
 
         /*
             @brief Updates the state of the rocket
-            @details This function is called every loop and updates the state of the rocket using the current sensor data
         */
-        void updateState(float accelX, float accelY, float accelZ, float gyroX, float gyroY, float gyroZ, float magX, float magY, float magZ, float baroAlt, float dt){
+        void updateState(int16_t Accel_X, int16_t Accel_Y, int16_t Accel_Z, int16_t Gyro_X, int16_t Gyro_Y, int16_t Gyro_Z, int32_t Mag_X, int32_t Mag_Y, int32_t Mag_Z, int32_t Baro_D1, int32_t Baro_D2, int16_t dt_MS){
             if (E_state == PRELAUNCH){
                 
             }
@@ -59,4 +56,10 @@ class StateEstimator{
                 
             }
         }
+        /*
+            @brief Updates the state of the rocket when on the pad
+            
+        */
+        void updateState_PreLaunch(float Accel_X, float Accel_Y, float Accel_Z, float Gyro_X, float Gyro_Y, float Gyro_Z, float Mag_X, float Mag_Y, float Mag_Z, float Baro_Pressure, float dt_MS);
+        void updateState_PostLaunch(float Accel_X, float Accel_Y, float Accel_Z, float Gyro_X, float Gyro_Y, float Gyro_Z, float Mag_X, float Mag_Y, float Mag_Z, float Baro_Pressure, float dt_MS);
 };
