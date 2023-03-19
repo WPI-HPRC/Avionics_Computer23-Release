a85b276bb329af8b7e89738c18f22ff064e3e22d
Abby Hyde <abbyghyde@gmail.com>
2023-03-19 13:41:14 -0400

    added state machine and stuff from test launch

--- ControllerBoard/src/ControllerBoardConstants.h
+++ ControllerBoard/src/ControllerBoardConstants.h
@@ -6,3 +6,6 @@ const int HZ_100 = 100;
 const int CONVERSION = 1000;
 // Set this to the desired Hz for the loop
 const int LOOP_FREQUENCY = HZ_100;
+
+const int MOTOR_BURNOUT_INTERVAL = 0;
+const int PRELAUNCH_INTERVAL = 1000;
